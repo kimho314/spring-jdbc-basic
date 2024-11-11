a504bd38facaafb14c77a969918f297a61ed1005
Hoseop.Kim <kimho314@gmail.com>
2024-11-11 21:50:40 +0900

    init project

---
+++ src/main/java/com/example/basic_jdbc/BasicJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.basic_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basic_jdbc/BasicJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basic_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
