3aac441fbba59eff015188540945008d2ffa9085
Hoseop.Kim <kimho314@gmail.com>
2024-12-15 16:14:24 +0900

    [feat] : add printEx testcase

--- src/test/java/com/example/basic_jdbc/exception/basic/UncheckedAppTest.java
+++ src/test/java/com/example/basic_jdbc/exception/basic/UncheckedAppTest.java
@@ -1,10 +1,12 @@
 package com.example.basic_jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+@Slf4j
 public class UncheckedAppTest {
 
     @Test
@@ -12,6 +14,17 @@ public class UncheckedAppTest {
         Controller controller = new Controller();
         Assertions.assertThatThrownBy(() -> controller.request())
                 .isInstanceOf(Exception.class);
+    }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        }
+        catch (Exception e) {
+            log.info("ex", e);
+        }
     }
 
     private static class Controller {
