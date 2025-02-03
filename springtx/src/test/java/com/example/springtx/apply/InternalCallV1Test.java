174447d55dc447d1760e538ce2b25b5684cd3f52
Hoseop.kim <kimho314@naver.com>
2025-02-03 20:43:36 +0900

    [feat] : add InternalCallV1Test

---
+++ springtx/src/test/java/com/example/springtx/apply/InternalCallV1Test.java
@@ -0,0 +1,63 @@
+package com.example.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV1Test {
+
+    @Autowired
+    CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class={}", callService.getClass());
+    }
+
+    @Test
+    void internalCall() {
+        callService.internal();
+    }
+
+    @Test
+    void externalCall() {
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1TstConfig {
+
+        @Bean
+        CallService callService() {
+            return new CallService();
+        }
+    }
+
+    static class CallService {
+
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+        }
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", isActive);
+            boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("tx readOnly={}", isReadOnly);
+        }
+    }
+}
