f9ec1b24014f7d5439315bd9f463518f4d62b649
Hoseop.kim <kimho314@naver.com>
2025-02-07 20:26:48 +0900

    [feat] : add inner_rollback method

--- springtx/src/test/java/com/example/springtx/propagation/BasicTxTest.java
+++ springtx/src/test/java/com/example/springtx/propagation/BasicTxTest.java
@@ -2,12 +2,14 @@ package com.example.springtx.propagation;
 
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.IllegalTransactionStateException;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
@@ -79,7 +81,7 @@ public class BasicTxTest {
     }
 
     @Test
-    void outer_commit() {
+    void outer_rollback() {
         log.info("외부 트랝개션 시작");
         TransactionStatus outer = txManager.getTransaction(new DefaultTransactionDefinition());
         log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
@@ -90,7 +92,27 @@ public class BasicTxTest {
         log.info("내부 트랜잭션 커밋");
         txManager.commit(inner);
 
-        log.info("외부 트랜잭션 커밋");
+        log.info("외부 트랜잭션 롤백");
         txManager.rollback(outer);
+    }
+
+
+    @Test
+    void inner_rollback() {
+        log.info("외부 트랝개션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(
+            new DefaultTransactionDefinition()); //Transaction rolled back because it has been marked as rollback-only
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner);
+
+        log.info("외부 트랜잭션 커밋");
+//        txManager.commit(outer);
+        Assertions.assertThatThrownBy(() -> txManager.commit(inner))
+            .isInstanceOf(IllegalTransactionStateException.class);
     }
 }
