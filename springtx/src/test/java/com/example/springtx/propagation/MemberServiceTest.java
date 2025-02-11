cd5729f23e9383a5b5b8a19947127a64d843bfdd
Hoseop.kim <kimho314@naver.com>
2025-02-11 22:14:08 +0900

    [feat] : add outerTxOn_fail

--- springtx/src/test/java/com/example/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/com/example/springtx/propagation/MemberServiceTest.java
@@ -53,4 +53,56 @@ class MemberServiceTest {
         Assertions.assertThat(memberRepository.find(userName).isPresent()).isTrue();
         Assertions.assertThat(logRepository.find(userName).isPresent()).isFalse();
     }
+
+    /**
+     * memberService        @Transactional:off
+     */
+    @Test
+    void singleTx() {
+        //given
+        String userName = "singleTx";
+
+        //when
+        memberService.joinV1(userName);
+
+        //then
+        Assertions.assertThat(memberRepository.find(userName).isPresent()).isTrue();
+        Assertions.assertThat(logRepository.find(userName).isPresent()).isTrue();
+    }
+
+    /**
+     * memberService        @Transactional:off
+     */
+    @Test
+    void outerTxOn_success() {
+        //given
+        String userName = "outerTxOn_success";
+
+        //when
+        memberService.joinV1(userName);
+
+        //then
+        Assertions.assertThat(memberRepository.find(userName).isPresent()).isTrue();
+        Assertions.assertThat(logRepository.find(userName).isPresent()).isTrue();
+    }
+
+    /**
+     * memberService        @Transactional:on
+     * memberRepository     @Transactional:on
+     * logRepository        @Transactional:on Exception
+     */
+    @Test
+    void outerTxOn_fail() {
+        //given
+        String userName = "로그예외_outerTxOn_fail";
+
+        //when
+        Assertions.assertThatThrownBy(() -> memberService.joinV1(userName))
+            .isInstanceOf(RuntimeException.class);
+
+        //then
+        // 모든 데이터가 롤백 된다.
+        Assertions.assertThat(memberRepository.find(userName).isPresent()).isFalse();
+        Assertions.assertThat(logRepository.find(userName).isPresent()).isFalse();
+    }
 }
